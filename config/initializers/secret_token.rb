20a27a63713a969b484afda96ef3a0756195b16f
tyler liu <replicat0r.misc@gmail.com>
2015-03-04 23:24:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tknewsdownloader::Application.config.secret_key_base = '77e0b023c430a1054f493972c2123e8a10b41ccb91c6570c91ab220e3521396dd1e808121b2360c168c134b3e41ef20dbc3a3327248c22ff3538714fb4530154'
